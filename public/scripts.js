f1968a2061805a7d8eb56be918fafae93ae4128a
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2024-09-24 08:32:48 +0300

    viestit näkyvät chatboxissa

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,27 @@
 document.getElementById('chatbot-send-button').addEventListener('click', sendChatMessage);
 
+document.getElementById('chatbot-user-input').addEventListener('keypress', function (keyPressed) {
+    if (keyPressed.key === 'Enter') {
+        sendChatMessage();
+    }
+  });
+  
+
 function sendChatMessage() {
     console.log('viesti lähetetään');
+    var userChatMessage = document.getElementById('chatbot-user-input').value;
+    console.log(userChatMessage);
+    document.getElementById('chatbot-user-input').value = '';
+    addMessageToChatbox(userChatMessage);
 }
+
+function addMessageToChatbox(message){
+    console.log('viesti lisätään chatboxiin');
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    console.log(messageElement);
+    document.getElementById('chatbox').appendChild(messageElement);
+
+
+}
+    
